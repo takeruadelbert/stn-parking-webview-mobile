e6f6b3c188fd3917a2bafdcd98b8d812500e0a4d
Lukas Fam <takeruadelbert@gmail.com>
2020-08-24 06:04:39 +0700

    added pull-down-refresh feature

--- src/screen/STNParkingWebView.js
+++ src/screen/STNParkingWebView.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
+import React, {Component, createRef} from 'react';
+import {View, ScrollView, RefreshControl} from 'react-native';
 import {WebView} from 'react-native-webview';
 import {MAIN_LAUNCH_URI} from '../tools/constant';
 import Style from './style';
@@ -7,13 +7,36 @@ import Style from './style';
 class STNParkingWebView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      refreshing: false,
+    };
+    this.webViewRef = createRef();
   }
+
+  _toggleRefreshState = (isRefresh) => {
+    this.setState({refreshing: isRefresh});
+  };
+
+  _onRefresh = () => {
+    this._toggleRefreshState(true);
+    this.webViewRef.current.reload();
+    this._toggleRefreshState(false);
+  };
 
   render() {
     return (
-      <View style={Style.container}>
-        <WebView source={{uri: MAIN_LAUNCH_URI}} />
-      </View>
+      <ScrollView
+        style={{flex: 1}}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }>
+        <View style={Style.container}>
+          <WebView source={{uri: MAIN_LAUNCH_URI}} ref={this.webViewRef} />
+        </View>
+      </ScrollView>
     );
   }
 }
