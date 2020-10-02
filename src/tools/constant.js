0938ceefc5b8457264466e6fb916ff347a866ab4
Lukas Fam <takeruadelbert@gmail.com>
2020-10-02 10:14:48 +0700

    update version & change main URI to domain production.

--- src/tools/constant.js
+++ src/tools/constant.js
@@ -1,4 +1,4 @@
-export const MAIN_LAUNCH_URI = 'http://103.39.49.183/login';
+export const MAIN_LAUNCH_URI = 'https://permata.sayaba.suryateknologi.com/';
 
 export const STORAGE_URL = 'https://storage.bunnycdn.com/materialdesignnative';
 export const ACCESS_KEY = 'ba8dd74c-5844-4d83-8523b4a5f50d-a128-4190';
