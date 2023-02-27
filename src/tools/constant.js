af490083ab8d6d676b9ce065e1b98bcaaf9ec9c2
Lukas Fam <lukas@samudrainovasi.co.id>
2023-02-27 20:46:06 +0700

    update base url for RS Guntur

--- src/tools/constant.js
+++ src/tools/constant.js
@@ -1,4 +1,4 @@
-export const MAIN_LAUNCH_URI = 'https://permata.sayaba.suryateknologi.com/';
+export const MAIN_LAUNCH_URI = 'https://guntur.suryateknologi.com/';
 
 export const STORAGE_URL = 'https://storage.bunnycdn.com/materialdesignnative';
 export const ACCESS_KEY = 'ba8dd74c-5844-4d83-8523b4a5f50d-a128-4190';
